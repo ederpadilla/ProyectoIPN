5b861a3b4cdf1f9398d1c2d16b3dcef14f6aa7eb
edersinsinsin <eder.padilla97@gmail.com>
2016-10-12 16:02:41 -0500

    Listo la actividad para agregar alumno

--- app/src/main/java/homecomingalpha/ederpadilla/example/com/proyectoipn/activitys/AgregarAlumnoActivity.java
+++ app/src/main/java/homecomingalpha/ederpadilla/example/com/proyectoipn/activitys/AgregarAlumnoActivity.java
@@ -3,6 +3,7 @@ package homecomingalpha.ederpadilla.example.com.proyectoipn.activitys;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import butterknife.ButterKnife;
 import homecomingalpha.ederpadilla.example.com.proyectoipn.R;
 
 public class AgregarAlumnoActivity extends AppCompatActivity {
@@ -11,5 +12,6 @@ public class AgregarAlumnoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_agregar_alumno);
+        ButterKnife.bind(this);
     }
 }
